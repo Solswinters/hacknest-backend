4ff7a13b1303f2dcd2243555e89823a453aa066e
Solswinters <jhustbright@gmail.com>
2025-11-17 21:19:33 +0100

    Add submissions module

---
+++ backend/src/submissions/submissions.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SubmissionsController } from './submissions.controller';
+import { SubmissionsService } from './submissions.service';
+import { Submission, SubmissionSchema } from './schemas/submission.schema';
+import { EventsModule } from '../events/events.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Submission.name, schema: SubmissionSchema },
+    ]),
+    EventsModule,
+  ],
+  controllers: [SubmissionsController],
+  providers: [SubmissionsService],
+  exports: [SubmissionsService],
+})
+export class SubmissionsModule {}
+
