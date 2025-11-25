7d4bdba8cdd65de040d242e84e0ee581d5e84320
Solswinters <jhustbright@gmail.com>
2025-11-25 22:50:50 +0100

    refactor(auto): standardize imports in jwt.strategy.ts

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -1,7 +1,9 @@
+import { ConfigService } from '@nestjs/config';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { ConfigService } from '@nestjs/config';
+
 import { UsersService } from '../../users/users.service';
 
 export interface JwtPayload {
