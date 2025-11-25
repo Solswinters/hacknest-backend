f2b7312fbd31c661606b4fba270d5aa66e98fdf1
Solswinters <jhustbright@gmail.com>
2025-11-25 22:51:26 +0100

    refactor(auto): standardize imports in judging.module.ts

--- backend/src/judging/judging.module.ts
+++ backend/src/judging/judging.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
+import { EventsModule } from '../events/events.module';
+import { JobsModule } from '../jobs/jobs.module';
 import { JudgingController } from './judging.controller';
 import { JudgingService } from './judging.service';
 import { SubmissionsModule } from '../submissions/submissions.module';
-import { EventsModule } from '../events/events.module';
-import { JobsModule } from '../jobs/jobs.module';
 
 @Module({
   imports: [SubmissionsModule, EventsModule, JobsModule],
