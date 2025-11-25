16c7486eff72954d597cfe2a08596d7bd15f376c
Solswinters <jhustbright@gmail.com>
2025-11-25 22:50:42 +0100

    refactor(auto): standardize imports in app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,16 @@
-import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import configuration from './config/configuration';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { EventsModule } from './events/events.module';
-import { SubmissionsModule } from './submissions/submissions.module';
-import { JudgingModule } from './judging/judging.module';
-import { Web3Module } from './web3/web3.module';
-import { JobsModule } from './jobs/jobs.module';
-import { WalletModule } from './wallet/wallet.module';
 import { HealthController } from './common/controllers/health.controller';
+import { JobsModule } from './jobs/jobs.module';
+import { JudgingModule } from './judging/judging.module';
+import { SubmissionsModule } from './submissions/submissions.module';
+import { UsersModule } from './users/users.module';
+import { WalletModule } from './wallet/wallet.module';
+import { Web3Module } from './web3/web3.module';
 
 @Module({
   imports: [
